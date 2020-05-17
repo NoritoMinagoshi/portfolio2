02d5cc2e5b22962257c2a53a50c36c49faebda98
NoritoMinagoshi <60051907+NoritoMinagoshi@users.noreply.github.com>
2020-05-17 19:35:12 +0900

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 window.onload = function(){
   // ナビから各種項目へスクロール
-  if (window.matchMedia( '(min-width: 767px)' ).matches) {
+  if (window.matchMedia( '(min-width: 768px)' ).matches) {
     $('a[href^="#"]').click(function(){
       var speed = 500;
       var href = $(this).attr("href");
@@ -13,12 +13,12 @@ window.onload = function(){
       return false;
     });
   };
-  if (window.matchMedia( '(max-width: 766px)' ).matches) {
+  if (window.matchMedia( '(max-width: 767px)' ).matches) {
     $('a[href^="#"]').click(function(){
       var speed = 500;
       var href = $(this).attr("href");
       var target = $(href == "#" || href == "" ? 'html' : href);
-      var adjust = -60 ;
+      var adjust = -59 ;
       var position = target.offset().top + adjust;
       $("html,body").animate({
         scrollTop:position
@@ -32,7 +32,7 @@ window.onload = function(){
     var pagetop = $('#page-top');
     pagetop.hide();
     $(window).scroll(function() {
-      if (window.matchMedia( '(min-width: 767px)' ).matches) {
+      if (window.matchMedia( '(min-width: 768px)' ).matches) {
         if ($(this).scrollTop() > 100) {
           pagetop.fadeIn();
         } else {
@@ -52,3 +52,19 @@ window.onload = function(){
     });
   });
 };
+
+// headerの色切り替え
+if (window.matchMedia( '(max-width: 767px)' ).matches) {
+  $(window).scroll(function() {
+    var top = $(window).scrollTop();
+    if (top > 80) {
+      $('header').css('background-color', 'rgba(0, 0, 0, .7)');
+      $('header .header-nav .nav-bar .nav-bar-line').css('background-color', '#fff');
+      $('header > a h1').css('color', '#fff');
+    } else {
+      $('header').css('background-color', 'transparent');
+      $('header .header-nav .nav-bar .nav-bar-line').css('background-color', '#000');
+      $('header > a h1').css('color', '#000');
+    }
+  });
+}
